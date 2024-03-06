e180425b926539f2ce782d25b121ba7b1d2bf8f5
Realtebo <mirkotebaldi@yahoo.it>
2024-03-06 15:31:41 +0100

    fine integrazione mongo [in memoria]

---
+++ mongoose/weather/model.ts
@@ -0,0 +1,7 @@
+import mongoose, {model} from "mongoose";
+import {WeatherInterface} from "./interface";
+import {WeatherSchema} from "./schema";
+
+export default mongoose.models.Weather ||
+    model<WeatherInterface>("Weather", WeatherSchema);
+    
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,29 +1,38 @@
-import {ApolloServer} from "@apollo/server";
-import {startServerAndCreateNextHandler} from "@as-integrations/next";
-import {resolvers} from "../../graphql/resolvers";
-import {typeDefs} from "../../graphql/schema";
-import {NextApiHandler, NextApiRequest, NextApiResponse} from "next";
+import { ApolloServer } from "@apollo/server"
+import { startServerAndCreateNextHandler } from "@as-integrations/next"
+import { resolvers } from "../../graphql/resolvers"
+import { typeDefs } from "../../graphql/schema"
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
+import dbConnect from "../../middleware/db-connect"
 
 //@ts-ignore
 const server = new ApolloServer({
     resolvers,
-    typeDefs
-});
+    typeDefs,
+})
 
-const handler = startServerAndCreateNextHandler(server);
+const handler = startServerAndCreateNextHandler(server)
 
 const allowCors =
-    (fn: NextApiHandler) => async (req: NextApiRequest, res: NextApiResponse) => {
-        res.setHeader("Allow", "POST");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "POST");
-        res.setHeader("Access-Control-Allow-Headers", "*");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
+    (fn: NextApiHandler) =>
+    async (req: NextApiRequest, res: NextApiResponse) => {
+        res.setHeader("Allow", "POST")
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        res.setHeader("Access-Control-Allow-Methods", "POST")
+        res.setHeader("Access-Control-Allow-Headers", "*")
+        res.setHeader("Access-Control-Allow-Credentials", "true")
 
         if (req.method === "OPTIONS") {
-            res.status(200).end();
+            res.status(200).end()
         }
-        return await fn(req, res);
-    };
+        return await fn(req, res)
+    }
 
-export default allowCors(handler);
+const connectDB =
+    (fn: NextApiHandler) =>
+    async (req: NextApiRequest, res: NextApiResponse) => {
+        await dbConnect()
+        return await fn(req, res)
+    }
+
+export default connectDB(allowCors(handler))
